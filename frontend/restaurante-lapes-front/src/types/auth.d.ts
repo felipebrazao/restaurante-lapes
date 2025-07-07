a85d37715887f1ac93bf2febb47c20cc90ba6848
Felipe Brazao <felipebrazao@outlook.com>
2025-07-06 21:57:00 -0300

    Criação inicial para o backend

---
+++ frontend/restaurante-lapes-front/src/types/auth.d.ts
@@ -0,0 +1,11 @@
+export interface LoginForm{
+    email: string;
+    senha: string;
+}
+
+export interface RegisterForm{
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
